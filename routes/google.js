192cc9d72c0dd39e0762c6850861e1931626bb36
Malizma333 <malizma333@gmail.com>
2024-06-22 17:00:52 -0500

    changed google auth to use env vars

--- routes/google.js
+++ routes/google.js
@@ -4,38 +4,33 @@ var { google } = require('googleapis');
 const dotenv = require('dotenv').config();
 
 const auth = new google.auth.GoogleAuth({
-    // apiKey: process.env.API_KEY,
-    keyFilename: './google_credentials.json',
+    credentials: {
+        type: 'service_account',
+        project_id: process.env.PROJECT_ID,
+        private_key_id: process.env.PRIVATE_KEY_ID,
+        private_key: process.env.PRIVATE_KEY,
+        client_email: process.env.CLIENT_EMAIL,
+        client_id: process.env.CLIENT_ID,
+        auth_uri: "https://accounts.google.com/o/oauth2/auth",
+        token_uri: "https://oauth2.googleapis.com/token",
+        auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
+        client_x509_cert_url: "https://www.googleapis.com/robot/v1/metadata/x509/" + process.env.CLIENT_EMAIL.replace('@', '%40'),
+        universe_domain: "googleapis.com"
+    },
     scopes: 'https://www.googleapis.com/auth/spreadsheets'
 });
-
-// router.get("/", async function(req, res, next) {
-//     try {
-//         const client = await auth.getClient();
-//         const sheets = await google.sheets({ version: 'v4', auth: client });        
-//         const rows = await sheets.spreadsheets.values.get({
-//             auth,
-//             spreadsheetId: process.env.GOOGLE_SHEET_ID,
-//             range: process.env.GOOGLE_SHEET_RANGE
-//         });
-//         res.send(rows.data.values);
-//     } catch(e) {
-//         console.info("[Google Sheet Service]", e.message);
-//         res.send("{}");
-//     }
-//     
-// });
 
 router.post("/", async function(req, res, next) {
     try {
         const client = await auth.getClient();
-        const sheets = google.sheets({ version: 'v4', auth: client });        
+        const sheets = google.sheets({ version: 'v4', auth: client });
         const rows = sheets.spreadsheets.values.append({
             auth,
             spreadsheetId: process.env.GOOGLE_SHEET_ID,
-            range: process.env.GOOGLE_SHEET_RANGE,
+            range: "A1",
             valueInputOption: "USER_ENTERED",
             resource: {
+                majorDimension: "ROWS",
                 values: [req.body]
             }
         });
@@ -43,7 +38,6 @@ router.post("/", async function(req, res, next) {
     } catch(e) {
         res.send({message: "Failed: " + e.message})
     }
-    
 });
 
 module.exports = router;
