6dc3672bcee27d84dc7f4d15dd9fea43bfcf5b5c
Malizma333 <malizma333@gmail.com>
2024-06-23 17:19:47 -0500

    edited config and fixed views

--- api/google.js
+++ api/google.js
@@ -14,8 +14,7 @@ const auth = new google.auth.GoogleAuth({
         auth_uri: "https://accounts.google.com/o/oauth2/auth",
         token_uri: "https://oauth2.googleapis.com/token",
         auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
-        client_x509_cert_url: "https://www.googleapis.com/robot/v1/metadata/x509/" + process.env.CLIENT_EMAIL.replace('@', '%40'),
-        universe_domain: "googleapis.com"
+        client_x509_cert_url: "https://www.googleapis.com/robot/v1/metadata/x509/" + process.env.CLIENT_EMAIL.replace('@', '%40')
     },
     scopes: 'https://www.googleapis.com/auth/spreadsheets'
 });
--- app.js
+++ app.js
@@ -12,8 +12,8 @@ var googleRouter = require("./api/google");
 var app = express();
 
 // view engine setup
-// app.set('views', path.join(__dirname, 'views'));
-// app.set('view engine', 'jade');
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
 
 app.use(cors());
 app.use(logger('dev'));
